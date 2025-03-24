5fa987ae8e3218091cdf27ba9982f3897bccb6e2
Baptiste Pillot <baptiste@pillot.fr>
2025-03-24 14:37:48 +0100

    Replace default exports with named exports

--- src/fastify-file-session-store.ts
+++ src/fastify-file-session-store.ts
@@ -4,8 +4,7 @@ import { readFile, unlink, writeFile } from 'fs/promises'
 
 const cache: Record<string, Session> = {}
 
-export { FileStore }
-export default class FileStore implements SessionStore
+export class FileStore implements SessionStore
 {
 
 	constructor(public directory: string)
